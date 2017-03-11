59f0c913905fcbeba5c3eaf65d6ef9b51f9ce325
rg3915 <rg3915@yahoo.com.br>
2017-03-11 20:17:38 -0300

    Novo layout

---
+++ js/main.js
@@ -0,0 +1,263 @@
+$(document).ready(function() {
+  $('a').tooltip({
+    placement: 'top'
+  });
+
+  $("#div1").click(function() {
+    $("#widget-body1").slideToggle("slow");
+  });
+
+  $("#toggle1").click(function() {
+    $("#widget-body1").slideToggle("slow");
+
+    $('#toggle1').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div2").click(function() {
+    $("#widget-body2").slideToggle("slow");
+  });
+
+  $("#toggle2").click(function() {
+    $("#widget-body2").slideToggle("slow");
+
+    $('#toggle2').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div3").click(function() {
+    $("#widget-body3").slideToggle("slow");
+  });
+
+  $("#toggle3").click(function() {
+    $("#widget-body3").slideToggle("slow");
+
+    $('#toggle3').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div4").click(function() {
+    $("#widget-body4").slideToggle("slow");
+  });
+
+  $("#toggle4").click(function() {
+    $("#widget-body4").slideToggle("slow");
+
+    $('#toggle4').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div5").click(function() {
+    $("#widget-body5").slideToggle("slow");
+  });
+
+  $("#toggle5").click(function() {
+    $("#widget-body5").slideToggle("slow");
+
+    $('#toggle5').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div6").click(function() {
+    $("#widget-body6").slideToggle("slow");
+  });
+
+  $("#toggle6").click(function() {
+    $("#widget-body6").slideToggle("slow");
+
+    $('#toggle6').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div7").click(function() {
+    $("#widget-body7").slideToggle("slow");
+  });
+
+  $("#toggle7").click(function() {
+    $("#widget-body7").slideToggle("slow");
+
+    $('#toggle7').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div8").click(function() {
+    $("#widget-body8").slideToggle("slow");
+  });
+
+  $("#toggle8").click(function() {
+    $("#widget-body8").slideToggle("slow");
+
+    $('#toggle8').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div9").click(function() {
+    $("#widget-body9").slideToggle("slow");
+  });
+
+  $("#toggle9").click(function() {
+    $("#widget-body9").slideToggle("slow");
+
+    $('#toggle9').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div10").click(function() {
+    $("#widget-body10").slideToggle("slow");
+  });
+
+  $("#toggle10").click(function() {
+    $("#widget-body10").slideToggle("slow");
+
+    $('#toggle10').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div11").click(function() {
+    $("#widget-body11").slideToggle("slow");
+  });
+
+  $("#toggle11").click(function() {
+    $("#widget-body11").slideToggle("slow");
+
+    $('#toggle11').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div12").click(function() {
+    $("#widget-body12").slideToggle("slow");
+  });
+
+  $("#toggle12").click(function() {
+    $("#widget-body12").slideToggle("slow");
+
+    $('#toggle12').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div13").click(function() {
+    $("#widget-body13").slideToggle("slow");
+  });
+
+  $("#toggle13").click(function() {
+    $("#widget-body13").slideToggle("slow");
+
+    $('#toggle13').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div14").click(function() {
+    $("#widget-body14").slideToggle("slow");
+  });
+
+  $("#toggle14").click(function() {
+    $("#widget-body14").slideToggle("slow");
+
+    $('#toggle14').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div15").click(function() {
+    $("#widget-body15").slideToggle("slow");
+  });
+
+  $("#toggle15").click(function() {
+    $("#widget-body15").slideToggle("slow");
+
+    $('#toggle15').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+  $("#div16").click(function() {
+    $("#widget-body16").slideToggle("slow");
+  });
+
+  $("#toggle16").click(function() {
+    $("#widget-body16").slideToggle("slow");
+
+    $('#toggle16').toggleClass(function() {
+      if ($(this).is('.fa fa-chevron-down')) {
+        return '.fa fa-chevron-up';
+      } else {
+        return '.fa fa-chevron-down';
+      }
+    })
+  });
+
+
+});
