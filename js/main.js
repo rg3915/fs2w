30284e90ca9779822d55ed16a0d559b090412b87
Sergio Crisostomo <sergiosbox@gmail.com>
2017-04-02 08:25:38 +0200

    jQuery polish

--- js/main.js
+++ js/main.js
@@ -3,27 +3,9 @@ $(document).ready(function() {
     placement: 'top'
   });
 
-  // // Iniciando as divs minimizadas
-  for (var i = 1; i <= 16; i++) {
-    $("#widget-body" + i).slideToggle("fast");
-  }
-
-  var widgetSlideToggle = function(e) {
-    var targetId = $(this).data('targetId');
-    var widgetBodyId = "#widget-body" + targetId;
-    var toggleId = "#toggle" + targetId;
-
-    $(widgetBodyId).slideToggle("slow");
-
-    $(toggleId).toggleClass(function() {
-      if ($(this).is('.fa fa-chevron-down')) {
-        return '.fa fa-chevron-up';
-      } else {
-        return '.fa fa-chevron-down';
-      }
-    });
-  };
-
-  $(".widget-trigger").click(widgetSlideToggle);
-
+  $(".widget-trigger").click(function(){
+	  var target = $(this).next();
+	  $(this).next().slideToggle('slow');
+	  $(this).closest('.widget-gray').find('.fa-chevron-up, .fa-chevron-down').toggleClass('fa-chevron-up fa-chevron-down');
+  });
 });
